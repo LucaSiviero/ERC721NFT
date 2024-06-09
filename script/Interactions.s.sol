2fb211c0566114e9d537ac0669c483857dd3eca5
LucaSiviero <lucasivi.998@gmail.com>
2024-06-09 20:14:33 +0200

    Deployed to Sepolia testnetgit add .! NFTs in da wallet boi

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,7 +7,7 @@ import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNFT is Script {
     string public constant DOG =
-        "ipfs://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
+        "ipfs://QmWVLzdcyAcc7AV8VMQ3h77G3bQASFxE7TeSBUxjMjsUjG";
 
     function mintNFTOnContract(address contractAddress) public {
         vm.startBroadcast();
