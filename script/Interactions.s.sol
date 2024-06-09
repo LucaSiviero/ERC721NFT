54e1737cf277d045e86cf26986dd5cc63aa76e3e
LucaSiviero <lucasivi.998@gmail.com>
2024-06-09 18:56:14 +0200

    NFT minting with IPFS

---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MintBasicNFT is Script {
+    string public constant DOG =
+        "ipfs://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
+
+    function mintNFTOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNFT(DOG);
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNFT",
+            block.chainid
+        );
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+}
---
+++ test/TestBasicNFT.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNFT.s.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+
+contract TestBasicNFT is Test {
+    DeployBasicNFT deployer;
+    BasicNFT basicNFT;
+    address public USER = makeAddr("USER");
+    string public constant DOG =
+        "ipfs://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        basicNFT = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Doggos";
+        string memory actualName = basicNFT.name();
+
+        assertEq(expectedName, actualName);
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNFT.mintNFT(DOG);
+
+        assert(basicNFT.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(DOG)) ==
+                keccak256(abi.encodePacked(basicNFT.tokenURI(0)))
+        );
+    }
+}
